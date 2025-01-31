4cc352574e9c7f773773eebd1fa1dbcc53dda133
Tiparraco11 <ericharr05@gmail.com>
2025-01-31 09:57:53 +0000

    Add files via upload

---
+++ heat_map_correlation.py
@@ -0,0 +1,14 @@
+# Import modules
+import matplotlib.pyplot as mp
+import pandas as pd
+import seaborn as sb
+
+# Import file with data
+df = pd.read_csv("heart.csv")
+
+def heat_map_correlation():
+    #print(data.corr(numeric_only=True))
+    dataplot = sb.heatmap(df.corr(numeric_only=True), cmap="YlGnBu", annot=True)
+    mp.show()
+
+print(heat_map_correlation())
