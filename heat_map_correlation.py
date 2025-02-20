acf4d1989999071979ba99ddbf476586454bbfac
Zivha <144683073+Z24B@users.noreply.github.com>
2025-02-20 16:17:39 +0000

    Update heat_map_correlation.py

--- heat_map_correlation.py
+++ heat_map_correlation.py
@@ -12,3 +12,24 @@ def heat_map_correlation():
     #print(data.corr(numeric_only=True))
     dataplot = sb.heatmap(df.corr(numeric_only=True), cmap="YlGnBu", annot=True)
     mp.show()
+
+
+'''OLD CODE FOR HEATMAP
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+def plot_correlation(df):
+    # Visualize correlations
+    correlation_matrix = df.corr()
+
+    plt.figure(figsize=(20, 15))
+    sns.heatmap(correlation_matrix, annot=True, fmt='.2f', cmap='coolwarm')
+    plt.title('Correlation Matrix of Heart Disease Dataset')
+    plt.show()
+
+def heat_map_correlation(df):
+    #print(data.corr(numeric_only=True))
+    dataplot = sb.heatmap(df.corr(numeric_only=True), cmap="YlGnBu", annot=True)
+    mp.show()
+    '''
